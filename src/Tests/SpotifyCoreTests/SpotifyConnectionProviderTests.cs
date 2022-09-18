adbf30f5733940e6468055c6a0ddcfcadd0e1f9d
Christos Karapasias <christos@marteco.nl>
2022-09-18 17:06:29 +0900

    fix broken tests

--- src/Tests/SpotifyCoreTests/SpotifyConnectionProviderTests.cs
+++ src/Tests/SpotifyCoreTests/SpotifyConnectionProviderTests.cs
@@ -1,8 +1,8 @@
+using AutoFixture;
+using CPlayerLib;
 using Eum.Cores.Spotify.Contracts;
-using Eum.Cores.Spotify.Contracts.IntermediateConnection;
 using Eum.Cores.Spotify.Factories;
 using NSubstitute;
-using Xunit;
 
 namespace SpotifyCoreTests;
 
@@ -11,19 +11,28 @@ public class SpotifyConnectionProviderTests
     private readonly ISpotifyConnectionProvider _sut;
     private readonly ISpotifyConnectionFactory _connectionFactory = Substitute.For<ISpotifyConnectionFactory>();
     private readonly ILoginCredentialsProvider _loginCredentialsProvider = Substitute.For<ILoginCredentialsProvider>();
-    private readonly INewSpotifyConnection _spotifyConnection = 
-        Substitute.For<INewSpotifyConnection>();
+
+    private readonly IFixture _fixture = new Fixture();
     public SpotifyConnectionProviderTests()
     {
-        _sut = new SpotifyTcpConnectionProvider(_loginCredentialsProvider, _connectionFactory);
+        _sut = new SpotifyConnectionProvider(_loginCredentialsProvider, _connectionFactory);
     }
     [Fact]
     public async Task If_AliveConnection_Return_ThatConnection()
     {
-        _connectionFactory.GetNewConnection()
-            .Returns(_ => _spotifyConnection);
-        var newConnection = await _sut.GetConnectionAsync(CancellationToken.None);
+        var loginCredentials = _fixture
+            .Build<LoginCredentials>()
+            .Create();
 
+        _loginCredentialsProvider.GetCredentials()
+            .Returns(loginCredentials);
+        
+        var connection = Substitute.For<ISpotifyConnection>();
+        
+        _connectionFactory.GetNewConnection(loginCredentials)
+            .Returns(_ => connection);
+        
+        var newConnection = await _sut.GetConnectionAsync(CancellationToken.None);
         var getConnection = await _sut.GetConnectionAsync(CancellationToken.None);
 
         Assert.Equal(newConnection.ConnectionId, getConnection.ConnectionId);
@@ -31,12 +40,20 @@ public class SpotifyConnectionProviderTests
     [Fact]
     public async Task If_NonAliveConnection_Return_NewConnection()
     {
-
-        _spotifyConnection.IsAlive.Returns(true);
-        _connectionFactory.GetNewConnection()
-            .Returns(_ => _spotifyConnection);
+        var loginCredentials = _fixture
+            .Build<LoginCredentials>()
+            .Create();
         
-        var getConnection = await _sut.GetConnectionAsync(CancellationToken.None);
-        Assert.Equal(getConnection.IsAlive, true);
+        _loginCredentialsProvider.GetCredentials()
+            .Returns(loginCredentials);
+
+        var connection = Substitute.For<ISpotifyConnection>();
+
+        _connectionFactory.GetNewConnection(loginCredentials)
+            .Returns(_ => connection);
+        
+        var getConnection = await 
+            _sut.GetConnectionAsync(CancellationToken.None);
+        Assert.Equal(getConnection.ConnectionId, connection.ConnectionId);
     }
 }
--- src/Tests/SpotifyCoreTests/SpotifyConnectionTest.cs
+++ src/Tests/SpotifyCoreTests/SpotifyConnectionTest.cs
@@ -1,3 +1,5 @@
+using AutoFixture;
+using CPlayerLib;
 using Eum.Cores.Spotify.Connection;
 using Eum.Cores.Spotify.Contracts;
 using Eum.Cores.Spotify.Factories;
@@ -7,23 +9,18 @@ namespace SpotifyCoreTests;
 
 public class SpotifyConnectionTest
 {
-    private readonly ITcpConnection _tcpConnection = Substitute.For<ITcpConnection>();
+    private readonly ITcpConnectionFactory _tcpConnectionFactory = Substitute.For<ITcpConnectionFactory>();
     private readonly IApResolver _apResolver = Substitute.For<IApResolver>();
+    private readonly IFixture _fixture = new Fixture();
+    private readonly LoginCredentials _loginCredentials;
+
     private readonly ISpotifyConnection _sut;
+    
     public SpotifyConnectionTest()
     {
-        _sut = new SpotifyTcpConnection(_apResolver);
+        _loginCredentials = _fixture.Build<LoginCredentials>()
+            .Create();
+        _sut = new SpotifyConnection(_apResolver, _loginCredentials, _tcpConnectionFactory);
     }
-
-    [Fact]
-    public async Task T()
-    {
-        const string host = "test";
-        const ushort port = 80;
-
-        _apResolver.GetClosestAccessPoint()
-            .Returns(_ => (host, port));
-        
-        await _sut.InstantiateConnectionAsync(CancellationToken.None);
-    }
+    
 }
